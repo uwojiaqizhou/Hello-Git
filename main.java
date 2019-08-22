61152431c15a855890c49920bb61bae3af7adfcd
uwojiaqizhou <jiaqizhou@outlook.com>
2019-08-21 23:09:10 -0400

    main function

---
+++ main.java
@@ -0,0 +1,14 @@
+ Volume in drive C is OSDisk
+ Volume Serial Number is B227-76D6
+
+ Directory of C:\Users\zhoujiaq\Documents\personal\javaprj1
+
+08/21/2019  11:08 PM    <DIR>          .
+08/21/2019  11:08 PM    <DIR>          ..
+08/19/2019  10:33 PM               900 Hello.class
+08/19/2019  10:23 PM               174 Hello.java
+08/21/2019  11:08 PM                 0 main.java
+08/21/2019  11:02 PM               521 master.txt
+08/21/2019  07:54 PM               470 README.txt
+               5 File(s)          2,065 bytes
+               2 Dir(s)  169,729,040,384 bytes free
